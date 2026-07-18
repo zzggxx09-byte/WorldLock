38234fe7331e1f11d50d47907a9c68c6a505154e
zzggxx09-byte <zzggxx09@gmail.com>
2026-07-18 16:02:18 +0300

    Update SingleWorldMod.java

--- src/main/java/com/example/singleworldmod/SingleWorldMod.java
+++ src/main/java/com/example/singleworldmod/SingleWorldMod.java
@@ -3,9 +3,9 @@ package com.example.singleworldmod;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 
-@Mod(modid = SingleWorldMod.MODID, name = SingleWorldMod.NAME, version = SingleWorldMod.VERSION,
-        clientSideOnly = true)
+@Mod(modid = SingleWorldMod.MODID, name = SingleWorldMod.NAME, version = SingleWorldMod.VERSION)
 public class SingleWorldMod {
 
     public static final String MODID = "singleworldmod";
@@ -13,7 +13,13 @@ public class SingleWorldMod {
     public static final String VERSION = "1.0.0";
 
     @Mod.EventHandler
+    public void preInit(FMLPreInitializationEvent event) {
+        ModConfig.init(event.getSuggestedConfigurationFile());
+    }
+
+    @Mod.EventHandler
     public void init(FMLInitializationEvent event) {
         MinecraftForge.EVENT_BUS.register(new ClientEventHandler());
+        MinecraftForge.EVENT_BUS.register(new CombatCooldownHandler());
     }
 }
